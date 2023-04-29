3bc21af822aa0a177533f19932230bcf5cd6d973
M <mrgmckenzie@gmail.com>
2023-04-29 12:20:20 +0100

    created country and user models

---
+++ models/country.py
@@ -0,0 +1,4 @@
+class Country:
+    def __init__(self, input_name):
+        #Country has property of name (string)
+        self.name = input_name
---
+++ models/user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, input_first_name:str, input_last_name:str):
+        #User has properties of a first name (first_name) and a last name (last_name)
+        #Both are of data type: string
+        self.first_name = input_first_name
+        self.last_name = input_last_name
