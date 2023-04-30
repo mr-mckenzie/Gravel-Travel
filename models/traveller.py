bc9d69983624201500edf3c43ab53794486a19e5
M <mrgmckenzie@gmail.com>
2023-04-30 13:29:16 +0100

    updated comment

--- models/traveller.py
+++ models/traveller.py
@@ -1,6 +1,6 @@
 class Traveller:
     def __init__(self, input_first_name:str, input_last_name:str, input_id = None):
-        #User has properties of a first name (first_name) and a last name (last_name)
+        #Traveller has properties of a first name (first_name) and a last name (last_name)
         #Both are of data type: string
         self.first_name = input_first_name
         self.last_name = input_last_name
