ffbe9879d7def3907e73a31101ee8c2529057543
M <mrgmckenzie@gmail.com>
2023-04-30 13:28:33 +0100

    added id properties to Traveller & Country classes

--- models/country.py
+++ models/country.py
@@ -1,4 +1,6 @@
 class Country:
-    def __init__(self, input_name):
+    def __init__(self, input_name, input_id = None):
         #Country has property of name (string)
         self.name = input_name
+        #id is an optional varaible (of data type int) and defaults to None
+        self.id = input_id
--- models/traveller.py
+++ models/traveller.py
@@ -1,8 +1,8 @@
 class Traveller:
-    def __init__(self, input_first_name:str, input_last_name:str):
+    def __init__(self, input_first_name:str, input_last_name:str, input_id = None):
         #User has properties of a first name (first_name) and a last name (last_name)
         #Both are of data type: string
         self.first_name = input_first_name
         self.last_name = input_last_name
-
-    # WILL HAVE TO UPDATE THE NAME OF THE CLASS FROM USER TO TRAVELLER BECAUSE OF SQL KEYWORD
+        #id is an optional varaible (of data type int) and defaults to None
+        self.id = input_id
