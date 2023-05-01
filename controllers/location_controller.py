a9e22f457c4b3c8264d707771dc0728cb9c87ced
M <mrgmckenzie@gmail.com>
2023-05-01 15:45:24 +0100

    added front end for location and associated controller

--- controllers/location_controller.py
+++ controllers/location_controller.py
@@ -2,10 +2,12 @@
 from flask import Flask, render_template, Blueprint
 from models.location import Location
 import repositories.location_repository as location_repo
+import repositories.country_repository as country_repo
 
 locations_blueprint = Blueprint("locations", __name__)
 
 @locations_blueprint.route('/locations')
 def locations():
-    all_locations = location_repo.select_all
-    render_template('locations.jinja', input_locations = all_locations)
+    all_locations = location_repo.select_all()
+    print(all_locations)
+    return render_template('location/index.jinja', input_locations = all_locations)
