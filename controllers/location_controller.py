43d6b0066c5a5cf39adb5b5ba56d3c1b940b50cc
M <mrgmckenzie@gmail.com>
2023-05-02 12:14:04 +0100

    added functionality to display if location is on user's  wishlist

--- controllers/location_controller.py
+++ controllers/location_controller.py
@@ -4,6 +4,7 @@ from models.location import Location
 import repositories.location_repository as location_repo
 import repositories.country_repository as country_repo
 import repositories.holiday_repository as holiday_repo
+import repositories.wishlist_repository as wishlist_repo
 
 locations_blueprint = Blueprint("locations", __name__)
 
@@ -11,14 +12,16 @@ locations_blueprint = Blueprint("locations", __name__)
 def locations():
     all_locations = location_repo.select_all()
     all_holidays = holiday_repo.select_all()
+    wishlist = wishlist_repo.select_all()
     #print(all_locations)
     #print(all_holidays)
-    return render_template('locations/index.jinja', input_locations = all_locations, input_all_holidays = all_holidays)
+    return render_template('locations/index.jinja', input_locations = all_locations, input_all_holidays = all_holidays, input_wishlist = wishlist)
 
 @locations_blueprint.route('/locations/<id>')
 def single_location(id):
     one_location = location_repo.select_one(id)
     has_visited = holiday_repo.has_visited(id)
+    on_wishlist = wishlist_repo.on_wishlist(id)
     print(f'THIS IS ONE LOCATION: {one_location}')
-    return render_template('locations/single_location.jinja', input_location = one_location, has_visited = has_visited)
+    return render_template('locations/single_location.jinja', input_location = one_location, has_visited = has_visited, on_wishlist = on_wishlist)
     #return render_template('locations/index.jinja', input_locations = one_location)
--- repositories/wishlist_repository.py
+++ repositories/wishlist_repository.py
@@ -3,9 +3,37 @@ from db.run_sql import run_sql
 
 #import Location and Traveller classes
 from models.location import Location
+import repositories.location_repository as location_repo
 
 def save (input_location: Location):
     sql = 'INSERT INTO wishlist (location_id) VALUES (%s)'
     value = (input_location.id)
     run_sql(sql, value)
     return #print('SAVE SUCCESSFUL')
+
+def select_all():
+    sql = 'SELECT * FROM wishlist'
+    all_wishlist = run_sql(sql)
+    wishlist = [ ]
+    for row in all_wishlist:
+        row_location = location_repo.select_one(row[1])
+        wishlist.append(row_location)
+        #returns a list of wishlist location objects
+    return wishlist
+
+#def select_one(input_holiday_id):
+#    sql = 'SELECT * FROM holidays WHERE id = %s'
+#    value = str(input_holiday_id)
+#    return run_sql(sql, value)[0]
+
+#NEED FUNCTIONS TO DISPLAY VISITED FOR ONE TRAVELLER ID
+
+def on_wishlist(input_location_id):
+    sql = 'SELECT * FROM wishlist where location_id = (%s)'
+    result = run_sql(sql, str(input_location_id))
+    #print(result)
+    if result:
+        return True
+    else:
+        return False
+    
