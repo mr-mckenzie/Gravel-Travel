bdf2eaafd0c74f5ac87fea452c4052d3dfb89d8a
M <mrgmckenzie@gmail.com>
2023-05-02 12:43:03 +0100

    created user wishlist display

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask import Flask, render_template
 #import controllers
 from controllers.location_controller import locations_blueprint
 from controllers.country_controller import countries_blueprint
+from controllers.holidays_controller import holidays_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(locations_blueprint)
 app.register_blueprint(countries_blueprint)
+app.register_blueprint(holidays_blueprint)
 
 @app.route('/')
 def home():
---
+++ controllers/holidays_controller.py
@@ -0,0 +1,24 @@
+# import flask
+from flask import Flask, render_template, Blueprint
+from models.location import Location
+import repositories.location_repository as location_repo
+import repositories.country_repository as country_repo
+import repositories.holiday_repository as holiday_repo
+import repositories.wishlist_repository as wishlist_repo
+
+holidays_blueprint = Blueprint("holidays", __name__)
+
+@holidays_blueprint.route('/holidays')
+def show_all_holidays():
+    all_holidays = holiday_repo.select_all()
+    print(all_holidays)
+    return render_template('holidays/index.jinja', input_all_holidays = all_holidays)
+
+# @locations_blueprint.route('/holidays/<id>')
+# def single_holiday(id):
+#     one_location = location_repo.select_one(id)
+#     has_visited = holiday_repo.has_visited(id)
+#     on_wishlist = wishlist_repo.on_wishlist(id)
+#     print(f'THIS IS ONE LOCATION: {one_location}')
+#     return render_template('locations/single_location.jinja', input_location = one_location, has_visited = has_visited, on_wishlist = on_wishlist)
+#     #return render_template('locations/index.jinja', input_locations = one_location)
