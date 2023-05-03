e4b31d9ba8988cd760f4ec0a3c5e08d12ba81666
M <mrgmckenzie@gmail.com>
2023-05-03 14:58:24 +0100

    made minor updates and added comments

--- controllers/holidays_controller.py
+++ controllers/holidays_controller.py
@@ -1,35 +1,33 @@
-# import flask
 from flask import Flask, render_template, Blueprint, redirect, request
 import repositories.location_repository as location_repo
 import repositories.holiday_repository as holiday_repo
 
 holidays_blueprint = Blueprint("holidays", __name__)
 
+#display all holiday records
 @holidays_blueprint.route('/holidays')
 def show_all_holidays():
     all_holidays = holiday_repo.select_all()
     print(all_holidays)
     return render_template('holidays/index.jinja', input_all_holidays = all_holidays)
 
+#delete a holiday
 @holidays_blueprint.route('/holidays/<id>/delete', methods=['POST'])
 def delete_entry(id):
     holiday_repo.delete_by_id(id)
     return redirect('/holidays')
 
+#go to add holiday form
 @holidays_blueprint.route('/holidays/add', methods=['GET'])
 def show_form():
-    #all_countries = country_repo.select_all()
     all_locations = location_repo.select_all()
     return render_template('/holidays/add.jinja', input_locations = all_locations)
 
+#save a holiday via 'add holiday' form
 @holidays_blueprint.route('/holidays/add', methods=['POST'])
 def add_holiday():
-
     location_id = request.form['location_id']
     date_visited = request.form['date']
-
     location_visited = location_repo.select_one(location_id)
-
     holiday_repo.save(location_visited, date_visited)
-
     return redirect('/holidays')
--- controllers/location_controller.py
+++ controllers/location_controller.py
@@ -1,7 +1,5 @@
-# import flask
 from flask import Flask, render_template, Blueprint, redirect, request
 from models.location import Location
-from models.country import Country
 import repositories.location_repository as location_repo
 import repositories.country_repository as country_repo
 import repositories.holiday_repository as holiday_repo
@@ -9,6 +7,7 @@ import repositories.wishlist_repository as wishlist_repo
 
 locations_blueprint = Blueprint("locations", __name__)
 
+#display all locations
 @locations_blueprint.route('/locations')
 def locations():
     all_locations = location_repo.select_all()
@@ -18,12 +17,15 @@ def locations():
     for location in all_locations:
         has_visited = holiday_repo.has_visited(location.id)
         on_wishlist = wishlist_repo.on_wishlist(location.id)
-        location_with_visit_and_wishlist.append({'location':location, 
-                                                 'has_visited':has_visited,
-                                                 'on_wishlist':on_wishlist})
+        location_with_visit_and_wishlist.append(
+            {'location':location, 
+            'has_visited':has_visited,
+            'on_wishlist':on_wishlist}
+            )
 
     return render_template('locations/index.jinja', input_locations = location_with_visit_and_wishlist)
 
+#display single location record
 @locations_blueprint.route('/locations/<id>')
 def single_location(id):
     one_location = location_repo.select_one(id)
@@ -32,8 +34,8 @@ def single_location(id):
     holidays = holiday_repo.select_by_location(id)
     print(f'THIS IS ONE LOCATION: {one_location}')
     return render_template('locations/single_location.jinja', input_location = one_location, has_visited = has_visited, on_wishlist = on_wishlist, holidays = holidays)
-    #return render_template('locations/index.jinja', input_locations = one_location)
 
+#delete location record
 @locations_blueprint.route('/locations/<id>/delete', methods=['POST'])
 def delete_location(id):
     country_id = location_repo.select_one(id).country.id
@@ -41,27 +43,25 @@ def delete_location(id):
     path = '/countries/'+str(country_id)
     return redirect(path)
 
+#go to 'add location' form
 @locations_blueprint.route('/locations/add', methods=['GET'])
 def show_form():
     all_countries = country_repo.select_all()
     return render_template('/locations/add.jinja', input_countries = all_countries)
 
+#submit 'add location' form
 @locations_blueprint.route('/locations/add', methods=['POST'])
 def add_country():
-
     new_location_name = request.form['name']
     #print(f'THIS IS NEW COUNTRY NAME: {new_location_name}')
     new_country_id = (request.form['country_id'])
     #print(f'THIS IS NEW COUNTRY INSTANCE: {new_country_id}')
-
     new_country = country_repo.select_one(new_country_id)
-
     new_location = Location(new_location_name, new_country)
-
     location_repo.save(new_location)
-
     return redirect('/locations')
 
+#add/remove location from wishlist
 @locations_blueprint.route('/locations/<id>/toggle_wishlist', methods=['POST'])
 def toggle_wishlist(id):
 
@@ -71,9 +71,9 @@ def toggle_wishlist(id):
         wishlist_repo.save(id)
 
     path = '/locations/'+str(id)
-
     return redirect(path)
 
+#delete holiday record
 @locations_blueprint.route('/locations/<location_id>/holiday/<holiday_id>/delete', methods=['POST'])
 def delete_holiday(location_id, holiday_id):
     holiday_repo.delete_by_id(holiday_id)
